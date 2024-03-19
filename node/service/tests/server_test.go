6fdfeaee7986efe7df624d95c72ec0e322c20a3e
pphboy <pphboy@aliyun.com>
2024-03-20 07:49:28 +0800

    upd:

--- node/service/tests/server_test.go
+++ node/service/tests/server_test.go
@@ -1,37 +1,28 @@
 package service
 
 import (
-	"io"
-	"log"
 	"os"
 	"os/exec"
 	"path/filepath"
+	"syscall"
 	"testing"
+	"time"
 )
 
 func TestCom(t *testing.T) {
-	ap, err := filepath.Abs("./a.sh")
+	ap, err := filepath.Abs("./pghttp")
 	if err != nil {
 		t.Fatal(err)
 	}
 	os.Chmod(ap, 0777)
 
 	t.Log(ap)
-	cmd := exec.Command("sh", ap)
-	go func() {
-		if err := cmd.Run(); err != nil {
-			log.Fatal(err)
-		}
-	}()
-	p, err := cmd.StdoutPipe()
+	cmd := exec.Command(ap, "&")
+	cmd.SysProcAttr = &syscall.SysProcAttr{Pdeathsig: syscall.SIGKILL}
 
-	if err != nil {
+	if err := cmd.Start(); err != nil {
 		t.Fatal(err)
 	}
 
-	r, err := io.ReadAll(p)
-	if err != nil {
-		t.Fatal(err)
-	}
-	t.Logf("%s", r)
+	time.Sleep(10 * time.Second)
 }
