9829ac30c9b6861ba716a7c2d9242834244c95af
pphboy <pphboy@aliyun.com>
2024-03-22 08:15:11 +0800

    test: for ServiceManager

--- node/modules/tests/service_test.go
+++ node/modules/tests/service_test.go
@@ -13,6 +13,24 @@ import (
 	"time"
 )
 
+func gg() (string, error) {
+
+	resp, err := http.Get("http://127.0.0.1:8081")
+	if err != nil {
+		log.Println("http get", err)
+		return "", err
+	}
+	bd, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Println("io read all", err)
+		return "", err
+	}
+	log.Printf("body: %s", bd)
+	defer resp.Body.Close()
+
+	return string(bd), nil
+}
+
 func TestRunnerApp(t *testing.T) {
 
 	ra := modules.NewRunnerApp(exec.Command("./pghttp"),
@@ -25,24 +43,6 @@ func TestRunnerApp(t *testing.T) {
 		ra.Start()
 		wg.Done()
 	}()
-
-	gg := func() (string, error) {
-
-		resp, err := http.Get("http://127.0.0.1:8081")
-		if err != nil {
-			log.Println("http get", err)
-			return "", err
-		}
-		bd, err := io.ReadAll(resp.Body)
-		if err != nil {
-			log.Println("io read all", err)
-			return "", err
-		}
-		t.Logf("body: %s", bd)
-		defer resp.Body.Close()
-
-		return string(bd), nil
-	}
 
 	wg.Add(1)
 	ctx, cancel := context.WithCancel(context.Background())
@@ -62,7 +62,7 @@ func TestRunnerApp(t *testing.T) {
 	go func() {
 		time.Sleep(5 * time.Second)
 		// 10 秒之后关闭服务器
-		err := ra.Stop()
+		err := ra.Close()
 		if err != nil {
 			log.Println("stop err:", err)
 		}
@@ -103,5 +103,61 @@ func TestRunnerApp(t *testing.T) {
 	}()
 
 	wg.Wait()
+}
 
+func TestServiceManager(t *testing.T) {
+	t.Log("黄帝崩，藏桥山")
+	mg := modules.NewAppDirector()
+
+	var wg sync.WaitGroup
+
+	na := &models.NodeApp{
+		NodeAppId:   "ggasdf12",
+		NodeAppName: "pghttp",
+	}
+
+	ee := exec.Command("./pghttp")
+
+	appSer := modules.NewRunnerApp(ee, na)
+	mg.AddService(appSer)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	wg.Add(1)
+	go func() {
+		time.Sleep(1 * time.Second)
+
+		res, err := gg()
+		if err != nil {
+			log.Fatal("http get, err:", err)
+		}
+
+		t.Log("http get:", res)
+		wg.Done()
+
+		if err := appSer.Close(); err != nil {
+			log.Fatal("appSer close ", err)
+		}
+
+		cancel()
+	}()
+
+	wg.Add(1)
+	go func() {
+		select {
+		case <-ctx.Done():
+		}
+		time.Sleep(3 * time.Second)
+
+		res, err := gg()
+		if err == nil {
+			log.Fatal("http get, err:", err)
+		}
+
+		log.Println("closed err", err)
+
+		t.Log("http get:", res)
+		wg.Done()
+	}()
+
+	wg.Wait()
 }
