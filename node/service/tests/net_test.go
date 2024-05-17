a6664cc1a56b97f1d4584f565e5db96fa6b64f0f
皮豪 <pphboy@qq.com>
2024-05-17 09:23:29 +0000

    add test for net

--- node/service/tests/net_test.go
+++ node/service/tests/net_test.go
@@ -1,1 +1,15 @@
 package service
+
+import (
+	"go-node/service"
+	"testing"
+)
+
+func TestNodeNter(t *testing.T) {
+	n := service.NewNodeNeter()
+	ip, err := n.Ip4ByNetSegment("127.0.0")
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log(ip.String())
+}
